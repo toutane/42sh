a9d0a2765b0f46d5980a99bab378b29f8f963bb6
Lucas Siauve <lucas.siauve@epita.fr>
2024-01-11 15:45:35 +0100

    arch: link module library to 42sh program

--- src/lexer/tests/test_lexer.c
+++
@@ -1,9 +0,0 @@
-#include <criterion/criterion.h>
-#include <criterion/internal/assert.h>
-#include <criterion/internal/test.h>
-#include <stdio.h>
-
-#include "../lexer.h"
-
-Test(try, test1) { cr_expect(add(3, 4) == 7); }
-Test(try, test2) { cr_expect(add(3, 4) == 8); }
