fc11bb1232c53d7a77870abecca1c3dac7be65e4
Matthieu Tirloy <matthieu.tirloy@epita.fr>
2024-01-16 21:57:07 +0100

    feat: prefix rule with ASSIGNMENT_WORD

--- src/parser/rule/parser_prefix.c
+++ src/parser/rule/parser_prefix.c
@@ -9,7 +9,13 @@ enum parser_status parse_redirection(struct ast **res, struct lexer *lexer);
  */
 enum parser_status parse_prefix(struct ast **res, struct lexer *lexer)
 {
-    if (parse_redirection(res, lexer) == PARSER_OK)
+    if (lexer_peek(lexer).type == TOKEN_ASSIGNMENT_WORD)
+    {
+        // TODO: Create node for ASSIGNMENT_WORD
+        // Put it in the AST
+        return PARSER_OK;
+    }
+    else if (parse_redirection(res, lexer) == PARSER_OK)
     {
         return PARSER_OK;
     }
