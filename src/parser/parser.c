ae05480ae4f1648a798975c7b44a48dce18e1409
Lucas Siauve <90468509+lucassiauve@users.noreply.github.com>
2024-01-18 19:32:44 +0100

    Update parser.c
    refactor(parser): remove function definition in source file

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,7 +1,5 @@
 #include "parser.h"
 
-enum parser_status parse_input(struct ast **res, struct lexer *lexer);
-
 // juste call the parse_input rull
 enum parser_status parse(struct ast **res, struct lexer *lexer)
 {
