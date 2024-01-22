eea2538a95c6f814c8cfb31375654af7c7a762e7
Lucas Siauve <lucas.siauve@epita.fr>
2024-01-22 14:09:25 +0100

    arch: create a file test for each redirection

--- tests/functionals-tests/categories/redirection/testsuite.sh
+++ tests/functionals-tests/categories/redirection/testsuite.sh
@@ -1,1 +1,7 @@
-run_test_line redir1.test
+run_test_line redir_DGREAT.test
+run_test_line redir_DIVERS.test
+run_test_line redir_GREATAND.test
+run_test_line redir_GREAT.test
+run_test_line redir_LESSAND.test
+run_test_line redir_LESSGREAT.test
+run_test_line redir_LESS.test
