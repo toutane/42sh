aae4638a3622539d50532a3f0d3589a2999a2634
Armand Thibaudon <armand.thibaudon@epita.fr>
2024-01-22 14:11:13 +0100

    test: addition of pipe + redir tests

--- tests/functionals-tests/categories/pipeline/testsuite.sh
+++ tests/functionals-tests/categories/pipeline/testsuite.sh
@@ -1,2 +1,3 @@
 run_test_line pipeline1.test
+run_test_line pipe_and_redir.test
 # run_test_file pipeline2.test
--- tests/functionals-tests/categories/redirection/testsuite.sh
+++ tests/functionals-tests/categories/redirection/testsuite.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 run_test_line redir_DGREAT.test
 run_test_line redir_DIVERS.test
 run_test_line redir_GREATAND.test
@@ -5,3 +6,7 @@ run_test_line redir_GREAT.test
 run_test_line redir_LESSAND.test
 run_test_line redir_LESSGREAT.test
 run_test_line redir_LESS.test
+=======
+run_test_line redir1.test
+run_test_line redir2.test
+>>>>>>> Stashed changes
