43ffab95ea082a242aa2ec03940342fe1c4a43f2
Lucas Siauve <lucas.siauve@epita.fr>
2024-01-23 17:07:05 +0100

    test: add test for and or operators

--- src/execution/exec_and_or.c
+++ src/execution/exec_and_or.c
@@ -5,8 +5,12 @@ int eval_and(struct ast *ast, struct hash_map *gv_hash_map)
 {
     struct ast_and_or *ast_and_or = (struct ast_and_or *)ast;
 
-    return (!eval_ast(ast_and_or->left, gv_hash_map))
-        && (!eval_ast(ast_and_or->right, gv_hash_map));
+    int left_res = eval_ast(ast_and_or->left, gv_hash_map);
+    if (left_res != 0)
+    {
+        return left_res;
+    }
+    return eval_ast(ast_and_or->right, gv_hash_map);
 }
 
 // eval or node
@@ -14,6 +18,9 @@ int eval_or(struct ast *ast, struct hash_map *gv_hash_map)
 {
     struct ast_and_or *ast_and_or = (struct ast_and_or *)ast;
 
-    return (!eval_ast(ast_and_or->left, gv_hash_map))
-        || (!eval_ast(ast_and_or->right, gv_hash_map));
+    if (eval_ast(ast_and_or->left, gv_hash_map) == 0)
+    {
+        return 0;
+    }
+    return eval_ast(ast_and_or->right, gv_hash_map);
 }
---
+++ tests/functionals-tests/categories/and-or/testsuite.sh
@@ -0,0 +1,2 @@
+run_test_line simple_and-or.test
+run_test_line command_and-or.test
