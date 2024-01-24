2e51e370f1dcd38d86f4b946b305a3a5416e2605
Lucas Siauve <lucas.siauve@epita.fr>
2024-01-24 16:00:47 +0100

    test: add exit test created with TDD programming methode

--- tests/functionals-tests/categories/builtins/testsuite.sh
+++ tests/functionals-tests/categories/builtins/testsuite.sh
@@ -4,3 +4,5 @@ run_test_file indecisive_true.test
 run_test_file indecisive_false.test 
 run_test_file fuck_malloc1.test
 run_test_file fuck_malloc2.test
+run_test_line simple_exit.test
+run_test_line complex_exit.test
