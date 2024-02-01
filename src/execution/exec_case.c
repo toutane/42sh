cecbbc8ff1097beefd0dd0e2d1403cb2477ef0b8
Armand Thibaudon <armand.thibaudon@epita.fr>
2024-02-01 19:01:51 +0100

    test: addition of case test

--- src/execution/exec_case.c
+++ src/execution/exec_case.c
@@ -1,15 +1,29 @@
+#define _XOPEN_SOURCE 500
+
+#include <fnmatch.h>
+
 #include "execution/exec.h"
 
-static int is_in_argv(int argc, char **argv, char *word)
+static int is_in_argv(int argc, char **argv, char *word, struct mem *mem)
 {
+    char *expanded_word = expand_string(&(word), mem);
+
+    char *temp;
     for (int i = 0; i < argc; ++i)
     {
-        if (!strcmp(argv[i], word))
+        temp = expand_string(&(argv[i]), mem);
+
+        if (fnmatch(argv[i], expanded_word, 0) == 0)
         {
+            free(expanded_word);
+            free(temp);
             return 1;
         }
+
+        free(temp);
     }
 
+    free(expanded_word);
     return 0;
 }
 
@@ -22,7 +36,7 @@ int eval_case(struct ast *ast, struct mem *mem)
     for (int i = 0; i < ast_case->item_number; ++i)
     {
         temp = ((struct ast_case_item *)ast_case->cases_items[i]);
-        if (is_in_argv(temp->argc, temp->argv, ast_case->base_name))
+        if (is_in_argv(temp->argc, temp->argv, ast_case->base_name, mem))
         {
             return eval_ast(temp->compound_list, mem);
         }
--- tests/functionals-tests/categories/case/testsuite.sh
+++ tests/functionals-tests/categories/case/testsuite.sh
@@ -1,1 +1,11 @@
 run_test_file basic.test
+run_test_file simple_pattern_matching.test
+run_test_file no_body.test
+run_test_file no_compound_list.test
+run_test_file no_semicolon.test
+run_test_file case_in_for.test
+run_test_file asterix1.test
+run_test_file asterix2.test
+run_test_file asterix3.test
+run_test_file three_semicolon.test
+run_test_file three_semicolon2.test
