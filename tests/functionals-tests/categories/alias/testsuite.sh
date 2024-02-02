6fa90348e5ad2c3b6821d78d5e25713fc064ee03
Lucas Siauve <lucas.siauve@epita.fr>
2024-02-02 12:12:24 +0100

    test(alias): add alias test and new feat testsuite detect when segfault append

--- tests/functionals-tests/categories/alias/testsuite.sh
+++ tests/functionals-tests/categories/alias/testsuite.sh
@@ -6,3 +6,7 @@ run_test_file unalias_all.test
 run_test_file complex_alias.test
 run_test_file alias_mult_word_sub.test
 run_test_file alias_function_def.test
+run_test_file alias_quotes.test
+run_test_file void_alias.test
+run_test_file void_alias_single_quote.test
+run_test_file void_alias_double_quote.test
