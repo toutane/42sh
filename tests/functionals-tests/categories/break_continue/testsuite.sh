245ccd7de480719fb4d0231aab252d3fadea9102
Lucas Siauve <lucas.siauve@epita.fr>
2024-02-02 12:49:39 +0100

    test(break): add break test that not passing

--- tests/functionals-tests/categories/break_continue/testsuite.sh
+++ tests/functionals-tests/categories/break_continue/testsuite.sh
@@ -4,15 +4,16 @@ run_test_file break_two_loops2.test
 run_test_file break_more_than_exists.test
 run_test_file break_two_break.test
 run_test_file break_zero.test
-# run_test_file break_two_args.test
+run_test_file break_two_args.test
 run_test_file break_minus.test
-# run_test_file break_two_args_valid.test
-# run_test_file break_two_args_invalid.test
+run_test_file break_two_args_valid.test
+run_test_file break_two_args_invalid.test
 run_test_file break_tricky.test
-# run_test_file break_string.test
+run_test_file break_string.test
 run_test_file break_more.test
-run_test_file function_break.test
-# run_test_file function_break2.test
+#run_test_file function_break.test
+#run_test_file function_break2.test
+run_test_file function_break3.test
 
 run_test_file continue_basic.test
 run_test_file continue_two_loops.test
